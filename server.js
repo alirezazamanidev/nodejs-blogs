070d77d05f884f12d1711e199c049df26b454715
alireza <alirezazamanidev80@gmail.com>
2025-01-29 12:52:29 +0330

    fix:: get blogs

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const { parse } = require("url");
 
 const server = createServer(async (req, res) => {
     const url = parse(req.url, true);
-
+    
   if (url.pathname === "/api/blogs") {
     res.writeHead(200, { "content-type": "application/json" });
     const blogs = await BlogModel.findAll();
     res.write(JSON.stringify({ blogs }));
     res.end();
   } else if (url.pathname === "/api/blog" && url.query?.id) {
-    const blogId = parseInt(id);
+    const blogId = parseInt(url.query.id);
     if (isNaN(blogId)) {
       res.writeHead(400, { "content-type": "application/json" });
       return res.end(JSON.stringify({ // return res
@@ -21,7 +21,7 @@ const server = createServer(async (req, res) => {
       }));
     
     }
-    const blog = await BlogModel.findOne(id);
+    const blog = await BlogModel.findOne(url.query.id);
     if (!blog) {
       res.writeHead(404, { "content-type": "application/json" });
       return res.end(
@@ -36,7 +36,7 @@ const server = createServer(async (req, res) => {
   }
 });
 
-const PORT = 8000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`server running at port ${PORT}`);
 });
